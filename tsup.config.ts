40435861db1e839265133d398815d4297b691945
tnmod <tnmod2003@gmail.com>
2025-03-14 14:46:23 +0700

    🚀  Add type Province 🚀

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/index.ts'],
+	entry: {
+		index: 'src/index.ts',
+		provinces: 'src/provinces/index.ts',
+		districts: 'src/districts/index.ts',
+		communes: 'src/communes/index.ts',
+	},
 	clean: true,
 	format: ['cjs', 'esm'],
-	dts: true,
+	dts: {
+		entry: {
+			index: 'src/index.ts',
+			provinces: 'src/provinces/index.ts',
+			districts: 'src/districts/index.ts',
+			communes: 'src/communes/index.ts',
+		},
+	},
+	sourcemap: false, // ✅ Tránh tạo file .d.ts.map không cần thiết
+	splitting: false, // ✅ Tránh tách file không cần thiết
+	outDir: 'dist', // ✅ Đảm bảo xuất file vào thư mục dist
 	loader: {
 		'.json': 'json',
 	},
